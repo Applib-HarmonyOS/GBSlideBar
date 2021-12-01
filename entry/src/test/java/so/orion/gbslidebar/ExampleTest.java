67f9a1f697dc70db9c5302e57661b21dad4186b6
fazil-ozil <60657039+fazil-ozil@users.noreply.github.com>
2021-12-01 16:24:33 +0530

    Test file removed

--- entry/src/test/java/so/orion/gbslidebar/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package so.orion.gbslidebar;
-
-import org.junit.Test;
-
-public class ExampleTest {
-    @Test
-    public void onStart() {
-    }
-}
--- slidebar/src/test/java/so/orion/slidebar/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package so.orion.slidebar;
-
-import org.junit.Test;
-
-public class ExampleTest {
-    @Test
-    public void onStart() {
-    }
-}
